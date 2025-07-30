ab4927213f1c0ea218a326e74fa96dc7feac504c
Dhruv Ratan Jayaswal <dhruvratanjayaswal@Dhruvs-MacBook-Air.local>
2025-07-30 19:04:08 +0530

    Auth: Initialised

---
+++ octo/src/lib/auth.ts
@@ -0,0 +1,17 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import * as schema from "@/db/schema";
+
+import { db } from "@/db";
+
+export const auth = betterAuth({
+  emailAndPassword: {
+    enabled: true,
+  },
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    schema: {
+      ...schema,
+    },
+  }),
+});
